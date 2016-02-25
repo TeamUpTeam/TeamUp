16522eca686c520371a69f4511b235e799abbf72
rmantzoros <rmantzoros@gmail.com>
2016-02-25 01:40:06 -0500

    Fixed User, Project, and Task

--- TeamUp/app/src/main/java/com/teamup/teamup/Task.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/Task.java
@@ -1,4 +1,32 @@
 package com.teamup.teamup;
 
+/**
+ * Created by Robbie on 2/22/2016.
+ */
 public class Task {
+    public String taskName;
+    public String taskDescription;
+    public User claimedBy;
+    public String deadline;
+    public boolean approved;
+    public Task(String taskName, String taskDescription, User claimedBy, String deadline, boolean approved)
+    {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.claimedBy = claimedBy;
+        this.deadline = deadline;
+        this.approved = approved;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Task: " + taskName);
+        sb.append("\tDescription: " + taskDescription);
+        sb.append("\tClaimed by: " + claimedBy);
+        sb.append("\tDeadline: " + deadline);
+        sb.append("\tApproved: " + approved);
+        return sb.toString();
+    }
 }
--- TeamUp/app/src/main/java/com/teamup/teamup/User.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/User.java
@@ -1,11 +1,31 @@
 package com.teamup.teamup;
 
+import java.util.ArrayList;
+
+/**
+ * Created by Robbie on 2/22/2016.
+ */
 public class User {
-    public void AddUser() {
-        //Add code here
+    public String userName;
+    // public String email; if time permits
+    public ArrayList <Project> projectList = new ArrayList<>();
+
+    public User(String userName, ArrayList <Project> projectList)
+    {
+        this.userName = userName;
+        this.projectList = projectList;
     }
-    
-    public void RemoveUser() {
-        //Add code here
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Username: " + userName);
+        sb.append("\tProject List: ");
+        for(Project p:projectList) {
+            sb.append(p.projectName + ", ");
+        }
+
+        return sb.toString();
     }
 }

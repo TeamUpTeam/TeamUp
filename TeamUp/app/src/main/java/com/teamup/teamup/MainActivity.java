5da346618a144e8102bf1886d69ee114dc0e1d89
junejajai <juneja0@purdue.edu>
2016-02-29 00:32:05 -0500

    new project page

--- TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
@@ -9,22 +9,26 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //b = (Button)findViewById(R.id.fab);
+       // b.setOnClickListener(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), ProjectActivity.class);
+                startActivity(intent);
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
             }
         });
     }
@@ -34,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        //attemptLogin(); //removed temporarily
+        Intent intent = new Intent(view.getContext(), ProjectActivity.class);
+        startActivity(intent);
     }
 
     @Override

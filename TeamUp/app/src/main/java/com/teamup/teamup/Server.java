529c0d440ff17fa4274c300e0d19cc4daa9dd1f0
Robbie <rmantzoros@gmail.com>
2016-03-03 16:22:09 -0500

    Fixed errors in setProjectMan

--- TeamUp/app/src/main/java/com/teamup/teamup/Server.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/Server.java
@@ -36,10 +36,9 @@ public class Server {
      * (String projectName, int projectID, String projectDescription, User teamLeader, ArrayList<User> teamMembers, ArrayList<Task> currentTasks, boolean TLaddMems, boolean TLaddTasks) {
 
      */
-    public int createProject (String pName, String pDescription, String creatorID, Context context)
+    public int createProject (String pName, String pDescription, Context context)
     {
         final String projectName = pName;
-        final String projectMangerUserID = creatorID;
         final String projectDescription = pDescription;
 
         String url = server_URL + "insert^into^project^(project_name,project_description)^values^('"+projectName+"','"+projectDescription+"');";
@@ -73,7 +72,9 @@ public class Server {
 
         return 0;
     }
-
+    /*
+     *  Sets the project_id and user_id in Project_Manager table
+     */
     public int setProjectMan(String creatorID, String projectID,Context context){
         
         final String project_ID = projectID;
@@ -82,33 +83,32 @@ public class Server {
         String url = server_URL + "insert^into^project_manager^(project_id,user_id)^values^('"+project_ID+"','"+projectMangerUserID+"');";
         RequestQueue queue = Volley.newRequestQueue(context);
         // Request a string response
-
-        // prepare the Request
-        JsonObjectRequest getRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        // the response is already constructed as a JSONObject!
-                        try {
-                            response = response.getJSONObject("args");
-                            String projectID = response.getString("project_id");
-                            Log.d("project_id: ",projectID);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
                     }
                 },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // Error handling
-                                Log.d("Error.Response", "Response Error" );
-                            }
-                        });
-
-        // add it to the RequestQueue
-        queue.add(getRequest);
-
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", "Server Response Error" );
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<>();
+                // the POST parameters:
+                params.put("project_id", project_ID);
+                params.put("user_id", projectMangerUserID);
+                return params;
+            }
+        };
+        queue.add(postRequest);
+        return 0;
     }
 
 

c3ce29b3ac0f070e0d6992423707411ddd0585fe
Shreyaansh Bassi <sbassi@purdue.edu>
2016-03-18 00:06:27 -0400

    Random Changes

--- TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
@@ -90,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
                                     listViewProj.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+
                                         @Override
                                         public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
                                             Intent i = new Intent(

523f270e12bcaa486fe3bc0cab22d16bf8a32561
junejajai <juneja0@purdue.edu>
2016-04-03 20:55:14 -0400

    working settings

--- TeamUp/app/src/main/java/com/teamup/teamup/SettingsActivity.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/SettingsActivity.java
@@ -36,6 +36,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,14 +56,95 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class SettingsActivity extends AppCompatActivity{
+String email = LoginActivity.currEmail;
+    TextView fn;
+    TextView ln;
+    TextView em;
+    TextView log;
+    TextView pass;
 
+    Context context = this;
+    String user_info;
+    String login_name;
+    String first_name;
+    String last_name;
+    String email_curr;
+    String password;
+    int userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        RequestQueue queue = Volley.newRequestQueue(context);
+        fn = (TextView) this.findViewById(R.id.textView2);
+        ln = (TextView) this.findViewById(R.id.textView3);
+        log = (TextView) this.findViewById(R.id.textView4);
+        em = (TextView) this.findViewById(R.id.textView5);
+
+        String url = Server.server_URL + String.format("getuserinfo?email=%s",email);
+        JsonArrayRequest getRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //successful row return, so allow login
+                    user_info = response.toString();
+                       try {
+                           login_name = response.getJSONObject(0).getString("login_name");
+                           first_name = response.getJSONObject(0).getString("first_name");
+                           last_name = response.getJSONObject(0).getString("last_name");
+                           email_curr = response.getJSONObject(0).getString("email_address");
+                           //password = response.getJSONObject(0).getString("password");
+                           //Log.d("fn %s",first_name);
+                           //Log.d("ln ",last_name);
+                           //Log.d("lognam ", login_name);
+                           //Log.d("email is ", email_curr);
+                           //String temp1 = first_name;
+                           fn.setText(response.getJSONObject(0).getString("first_name"));
+                           // }
+                           ln.setText(response.getJSONObject(0).getString("last_name"));
+
+                           log.setText(response.getJSONObject(0).getString("login_name"));
+
+                           em.setText(response.getJSONObject(0).getString("email_address"));
+                       }
+                       catch (Exception e)
+                       {
+
+                       }
+
+                       //final String uid = String.format("%d",userid);
+                    //Log.d("user_info is %s",user_info);
+                    //Log.d("login_name is %s",login_name);
+
+
+
+
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // Error handling
+                                Log.d("Error.Response", error.toString() );
+
+
+                            }
+                        });
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
+        //if(fn.equals("undefined"))
+        //{
+           // fn = (TextView) this.findViewById(R.id.textView2);
+         //   fn.setText("Not provided");
+        //}
+        //else {
+
 
 
     }
+
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.

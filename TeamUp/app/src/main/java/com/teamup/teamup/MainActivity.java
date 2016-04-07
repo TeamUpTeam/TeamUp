70fda0b9d9499c8812a9a9909b71d7b7f25fa518
Robbie <rmantzoros@gmail.com>
2016-04-07 15:18:42 -0400

    Removed checkLogin from mainActivity

--- TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/MainActivity.java
@@ -411,4 +411,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+        public boolean checkLogin(int userId){
+            
+
+            return false;
+        }
 }

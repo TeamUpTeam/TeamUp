567e7cfff9566a5625862488c38a2a96a59bfa2a
Brian Chan <chan96@purdue.edu>
2016-04-14 10:44:02 -0400

    comleted new sign up page

--- TeamUp/app/src/main/java/com/teamup/teamup/SignupActivity.java
+++ TeamUp/app/src/main/java/com/teamup/teamup/SignupActivity.java
@@ -1,14 +1,29 @@
 package com.teamup.teamup;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -16,7 +31,9 @@ import butterknife.InjectView;
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
 
-    @InjectView(R.id.input_name) EditText _nameText;
+    @InjectView(R.id.input_fname) EditText _fnameText;
+    @InjectView(R.id.input_lname) EditText _lnameText;
+    @InjectView(R.id.input_username) EditText _usernameText;
     @InjectView(R.id.input_email) EditText _emailText;
     @InjectView(R.id.input_password) EditText _passwordText;
     @InjectView(R.id.input_confpassword) EditText _confpasswordText;
@@ -47,6 +64,10 @@ public class SignupActivity extends AppCompatActivity {
 
     public void signup() {
         Log.d(TAG, "Signup");
+        final Context currContext = this;
+        final View currView = ((Activity)currContext).getWindow().getDecorView().findViewById(android.R.id.content);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
 
         if (!validate()) {
             onSignupFailed();
@@ -61,20 +82,24 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
+        final String fname = _fnameText.getText().toString();
+        final String lname = _lnameText.getText().toString();
+        final String username = _usernameText.getText().toString();
+        final String email = _emailText.getText().toString();
+        final String password = _passwordText.getText().toString();
 
         // TODO: Implement your own signup logic here.
 
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
+                        signUp(username, fname, lname, email, password, currContext, currView, progressDialog);
+
                         // On complete call either onSignupSuccess or onSignupFailed
                         // depending on success
-                        onSignupSuccess();
+
                         // onSignupFailed();
-                        progressDialog.dismiss();
+                        //progressDialog.dismiss();
                     }
                 }, 3000);
     }
@@ -95,16 +120,32 @@ public class SignupActivity extends AppCompatActivity {
     public boolean validate() {
         boolean valid = true;
 
-        String name = _nameText.getText().toString();
+        String fname = _fnameText.getText().toString();
+        String lname = _lnameText.getText().toString();
+        String username = _usernameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
         String confirmpassword = _confpasswordText.getText().toString();
 
-        if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
+        if (fname.isEmpty() || fname.length() < 3) {
+            _fnameText.setError("at least 3 characters");
             valid = false;
         } else {
-            _nameText.setError(null);
+            _fnameText.setError(null);
+        }
+
+        if (lname.isEmpty() || lname.length() < 3) {
+            _lnameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _lnameText.setError(null);
+        }
+
+        if (username.isEmpty() || username.length() < 3) {
+            _usernameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _usernameText.setError(null);
         }
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
@@ -129,5 +170,76 @@ public class SignupActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    public void signUp(String username, String fname, String lname, final String email, String password, Context context, final View view, final ProgressDialog progressDialog)
+    {
+        String url;
+        //final String loginName = login_Name;
+        // prepare the Request
+        RequestQueue queue = Volley.newRequestQueue(context);
+
+        // this url is the query being sent to the database
+        url = Server.server_URL + String.format("newuser?username=%s&firstname=%s&lastname=%s&email=%s&password=%s", username, fname, lname, email, password);
+        Log.d("url: ", url);
+        JsonObjectRequest getRequest = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //successful row return, so allow login
+                        Log.d("newuser", response.toString());
+                        onSignupSuccess();
+                        progressDialog.dismiss();
+
+                        LoginActivity3.currEmail = email;
+                        Intent intent = new Intent(view.getContext(), MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // Error handling
+
+                                progressDialog.dismiss();
+
+                                if (error.toString().contains("success")) {
+                                    LoginActivity3.currEmail = email;
+                                    onSignupSuccess();
+                                    Intent intent = new Intent(view.getContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    return;
+                                } else if (error.toString().contains("UnknownHostException")) {
+                                    new AlertDialog.Builder(view.getContext())
+                                            //.setTitle("Delete entry")
+                                            .setMessage("Unable to contact server. Please check your connection.")
+                                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    // continue with delete
+                                                }
+                                            })
+                                            .setIcon(android.R.drawable.ic_dialog_alert)
+                                            .show();
+                                } else {
+                                    new AlertDialog.Builder(view.getContext())
+                                            .setTitle("Unable to create new user")
+                                            .setMessage("Username or email exists.")
+                                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    // continue with delete
+                                                }
+                                            })
+                                            .setIcon(android.R.drawable.ic_dialog_alert)
+                                            .show();
+                                }
+                                Log.d("Error.Response", error.toString() );
+                                onSignupFailed();
+
+                            }
+                        });
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
     }
 }
